addd07ac7db3ba217572bd09afce74c6002989ff
Hamza <hrahm002@campus.goldsmiths.ac.uk>
2024-10-16 17:52:29 +0100

    a small change 2.0

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8000;
 
 http.createServer(function(req, res) { 
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("This is going to be MyWeb page!, evrything will be said here");
+    res.end("This is going to be MyWeb page!, evrything will be said here blah blah blah");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 }); 
